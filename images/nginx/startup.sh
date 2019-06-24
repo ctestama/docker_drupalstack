ffb8a8a0391e91854edb67c8a06d31d64df186ad
Colton Testamarck <colt@asu.edu>
2019-06-24 15:27:51 -0700

    php config reworked

--- images/nginx/startup.sh
+++ images/nginx/startup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-if [ ! -f /etc/php/7.0/fpm/php-fpm.conf ]; then
-	rsync -ravz /tmp/fpm/* /etc/php/7.0/fpm/
+if [ ! -f /etc/php/7.2/fpm/php-fpm.conf ]; then
+	rsync -ravz /tmp/fpm/* /etc/php/7.2/fpm/
 	sleep 5s
 fi
 
-#service php7.0-fpm start
+#service php7.2-fpm start
 /usr/bin/supervisord
