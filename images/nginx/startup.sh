e77147cd7f5ed94ad4257fba35704c1b7848f458
Colton Testamarck <colton.testamarck@gmail.com>
2020-05-18 17:24:13 -0700

    focal upgrade

--- images/nginx/startup.sh
+++ images/nginx/startup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-if [ ! -f /etc/php/7.2/fpm/php-fpm.conf ]; then
-	rsync -ravz /tmp/fpm/* /etc/php/7.2/fpm/
+if [ ! -f /etc/php/7.4/fpm/php-fpm.conf ]; then
+	rsync -ravz /tmp/fpm/* /etc/php/7.4/fpm/
 	sleep 5s
 fi
 
-#service php7.2-fpm start
+#service php7.4-fpm start
 /usr/bin/supervisord
